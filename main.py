a9d42a8c7740763142173b24e3b6f80f2aa29532
Elijah Karori Orito <elijakarori23@gmail.com>
2024-05-14 22:26:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI, HTTPException, Depends
 from routers import users
 
 app = FastAPI()
-
-app.include_router(users.router)
 # Endpoint for user registration
+app.include_router(users.router)
+
